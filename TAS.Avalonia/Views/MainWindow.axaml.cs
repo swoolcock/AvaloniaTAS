55a21fe99a74e8b948e2b011bd79488b4b1a8c0d
Shane Woolcock <samahnub@gmail.com>
2022-12-31 17:42:17 +1030

    Focus editor whenever the window gains focus

--- TAS.Avalonia/Views/MainWindow.axaml.cs
+++ TAS.Avalonia/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using TAS.Avalonia.Controls;
 
 namespace TAS.Avalonia.Views;
 
@@ -7,5 +8,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        GotFocus += (_, _) => this.FindControl<EditorControl>("editor")?.editor.Focus();
     }
 }
