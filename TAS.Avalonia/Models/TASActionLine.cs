10d2d971bc86911cc8ccd6db0900b51393915b32
psyGamer <psygamer21@gmail.com>
2023-07-20 19:33:10 +0200

    Added feather support to TASActionLine

--- TAS.Avalonia/Models/TASActionLine.cs
+++ TAS.Avalonia/Models/TASActionLine.cs
@@ -7,25 +7,42 @@ public struct TASActionLine {
     public TASAction Actions;
     public int Frames;
 
+    public float? FeatherAngle;
+    public float? FeatherMagnitude;
+
     public static bool TryParse(string line, out TASActionLine value) {
         value = default;
         string[] tokens = line.Trim().Split(",", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
         if (tokens.Length == 0) return false;
 
-        if (!int.TryParse(tokens[0], out int frames)) return false;
-        TASAction actions = tokens.Skip(1).Aggregate(TASAction.None, (a, s) => {
-            if (string.IsNullOrWhiteSpace(s)) return a;
-            var actionForChar = TASActionExtensions.ActionForChar(s[0]);
-            return a | actionForChar;
-        });
+        if (!int.TryParse(tokens[0], out value.Frames)) return false;
 
-        value = new TASActionLine {
-            Frames = frames, Actions = actions,
-        };
+        for (int i = 1; i < tokens.Length; i++) {
+            var action = TASActionExtensions.ActionForChar(tokens[i][0]);
+
+            if (action == TASAction.FeatherAim && i + 1 < tokens.Length && float.TryParse(tokens[i + 1], out var angle)) {
+                value.FeatherAngle = angle;
+                i++;
+
+                if (i + 1 < tokens.Length && float.TryParse(tokens[i + 1], out var magnitude)) {
+                    value.FeatherMagnitude = magnitude;
+                    i++;
+                }
+            }
+
+            value.Actions |= action;
+        }
 
         return true;
     }
 
-    public override string ToString() =>
-        Actions.Sorted().Aggregate(Frames.ToString().PadLeft(MaxFramesDigits), (s, a) => s + "," + a.CharForAction());
+    public override string ToString() {
+        string frames = Frames.ToString().PadLeft(MaxFramesDigits);
+        string actions = Actions.Sorted().Aggregate("", (s, a) => $"{s},{a.CharForAction()}");
+        string featherAngle = FeatherAngle.HasValue ? $",{FeatherAngle.Value}" : string.Empty;
+        string featherMagnitude = FeatherMagnitude.HasValue ? $",{FeatherMagnitude.Value}" : string.Empty;
+
+        return $"{frames}{actions}{featherAngle}{featherMagnitude}";
+    }
+
 }
