d8efd85d322cdc2ece864c0a2e00e06948df11df
psyGamer <psygamer21@gmail.com>
2023-07-22 14:55:41 +0200

    Fixed not clamping feather values

--- TAS.Avalonia/Models/TASActionLine.cs
+++ TAS.Avalonia/Models/TASActionLine.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace TAS.Avalonia.Models;
 
 #nullable enable
@@ -21,7 +23,7 @@ public struct TASActionLine {
         string[] tokens = line.Trim().Split(",", StringSplitOptions.TrimEntries);
         if (tokens.Length == 0) return false;
 
-        if (!int.TryParse(tokens[0], out value.Frames)) return false;
+        if (!int.TryParse(tokens[0], CultureInfo.InvariantCulture, out value.Frames)) return false;
 
         for (int i = 1; i < tokens.Length; i++) {
             if (string.IsNullOrWhiteSpace(tokens[i])) continue;
@@ -49,21 +51,42 @@ public struct TASActionLine {
 
             // Parse feather angle/magnitude
             bool validAngle = true;
-            if (action == TASAction.FeatherAim && i + 1 < tokens.Length && (validAngle = float.TryParse(tokens[i + 1], out var _))) {
-                value.FeatherAngle = tokens[i + 1];
+            if (action == TASAction.FeatherAim && i + 1 < tokens.Length && (validAngle = float.TryParse(tokens[i + 1], CultureInfo.InvariantCulture, out float angle))) {
+                if (angle > 360.0f)
+                    value.FeatherAngle = "360";
+                else if (angle < 0.0f)
+                    value.FeatherAngle = "0";
+                else
+                    value.FeatherAngle = tokens[i + 1];
                 i++;
 
                 // Allow empty magnitude, so the comma won't get removed
                 bool validMagnitude = true;
-                if (i + 1 < tokens.Length && (string.IsNullOrWhiteSpace(tokens[i + 1]) || (validMagnitude = float.TryParse(tokens[i + 1], out var _)))) {
-                    value.FeatherMagnitude = tokens[i + 1];
+                if (i + 1 < tokens.Length && (string.IsNullOrWhiteSpace(tokens[i + 1]) || (validMagnitude = float.TryParse(tokens[i + 1], CultureInfo.InvariantCulture, out float _)))) {
+                    // Parse again since it might be an empty string
+                    if (float.TryParse(tokens[i + 1], CultureInfo.InvariantCulture, out float magnitude)) {
+                        if (magnitude > 1.0f)
+                            value.FeatherMagnitude = "1";
+                        else if (magnitude < 0.0f)
+                            value.FeatherMagnitude = "0";
+                        else
+                            value.FeatherMagnitude = tokens[i + 1];
+                    } else {
+                        value.FeatherMagnitude = tokens[i + 1];
+                    }
+
                     i++;
                 } else if (!validMagnitude && !ignoreInvalidFloats) {
                     return false;
                 }
-            } else if (!validAngle && i + 2 < tokens.Length && string.IsNullOrEmpty(tokens[i + 1]) && (validAngle = float.TryParse(tokens[i + 2], out var _))) {
+            } else if (!validAngle && i + 2 < tokens.Length && string.IsNullOrEmpty(tokens[i + 1]) && (validAngle = float.TryParse(tokens[i + 2], CultureInfo.InvariantCulture, out angle))) {
                 // Empty angle, treat magnitude as angle
-                value.FeatherAngle = tokens[i + 2];
+                if (angle > 360.0f)
+                    value.FeatherAngle = "360";
+                else if (angle < 0.0f)
+                    value.FeatherAngle = "0";
+                else
+                    value.FeatherAngle = tokens[i + 1];
                 i += 2;
             } else if (!validAngle && !ignoreInvalidFloats) {
                 return false;
