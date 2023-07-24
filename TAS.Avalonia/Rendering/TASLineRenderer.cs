bc0f04abedcc28f8788db0ad96d39372a099a505
psyGamer <psygamer21@gmail.com>
2023-07-24 14:25:49 +0200

    Display current line suffix

--- TAS.Avalonia/Rendering/TASLineRenderer.cs
+++ TAS.Avalonia/Rendering/TASLineRenderer.cs
@@ -1,9 +1,13 @@
 using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Documents;
 using Avalonia.Media;
 using Avalonia.Media.Immutable;
+using Avalonia.Threading;
 using AvaloniaEdit.Editing;
 using AvaloniaEdit.Rendering;
 using AvaloniaEdit.Utils;
+using System.Globalization;
 
 namespace TAS.Avalonia.Rendering;
 
@@ -12,18 +16,34 @@ internal class TASLineRenderer : IBackgroundRenderer {
 
     public KnownLayer Layer => KnownLayer.Background;
 
+    private const double LineSuffixRightOffset = 5.0;
+
     public TASLineRenderer(TextArea textArea) {
         _textArea = textArea;
+        (Application.Current as App).CelesteService.Server.StateUpdated += _ => {
+            Dispatcher.UIThread.Invoke(() => _textArea.TextView.InvalidateVisual());
+        };
     }
 
     public void Draw(TextView textView, DrawingContext drawingContext) {
-        var brush = new ImmutableSolidColorBrush(Themeing.CurrentLineColor);
+        var currentLineBrush = new ImmutableSolidColorBrush(Themeing.CurrentLineColor);
+        var playingFramerush = new ImmutableSolidColorBrush(Themeing.PlayingFrameColor);
 
-        int line = _textArea.Caret.Line - 1;
+        int currentLine = _textArea.Caret.Line - 1;
+        int playingLine = (Application.Current as App).CelesteService.CurrentLine;
 
         var pixelSize = PixelSnapHelpers.GetPixelSize(textView);
-        var rect = new Rect(0, textView.DefaultLineHeight * line - textView.ScrollOffset.Y, textView.Bounds.Width, textView.DefaultLineHeight);
+        double currentLineY = textView.DefaultLineHeight * currentLine - textView.ScrollOffset.Y;
+        var currentLineRect = new Rect(0, currentLineY, textView.Bounds.Width, textView.DefaultLineHeight);
+        drawingContext.FillRectangle(currentLineBrush, PixelSnapHelpers.PixelAlign(currentLineRect, pixelSize));
 
-        drawingContext.FillRectangle(brush, PixelSnapHelpers.PixelAlign(rect, pixelSize));
+        string suffix = (Application.Current as App).CelesteService.CurrentLineSuffix;
+        double playingLineX = textView.Bounds.Width - textView.WideSpaceWidth * suffix.Length - LineSuffixRightOffset;
+        double playingLineY = textView.DefaultLineHeight * playingLine - textView.ScrollOffset.Y;
+        var typeface = textView.CreateTypeface();
+        double emSize = TextElement.GetFontSize(textView);
+        var text = new FormattedText(suffix, CultureInfo.InvariantCulture, FlowDirection.LeftToRight,
+                                     typeface, emSize, playingFramerush);
+        drawingContext.DrawText(text, new Point(playingLineX, playingLineY));
     }
 }
