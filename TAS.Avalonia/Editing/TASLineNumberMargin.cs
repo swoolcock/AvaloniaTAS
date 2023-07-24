5e3aab605fd7a25659de72b6e782590083e315f6
psyGamer <psygamer21@gmail.com>
2023-07-24 13:39:37 +0200

    Use centralized themeing

--- TAS.Avalonia/Editing/TASLineNumberMargin.cs
+++ TAS.Avalonia/Editing/TASLineNumberMargin.cs
@@ -9,24 +9,20 @@ using AvaloniaEdit.Utils;
 namespace TAS.Avalonia.Editing;
 
 public class TASLineNumberMargin : LineNumberMargin {
-    private readonly IBrush _playingLineBrush;
-
-    public static readonly Color PlayingLineColor = Color.FromArgb(255, 241, 250, 140);
-
     public TASLineNumberMargin() {
         (Application.Current as App).CelesteService.Server.StateUpdated += _ => {
             Dispatcher.UIThread.Invoke(() => InvalidateVisual());
         };
-        _playingLineBrush = new ImmutableSolidColorBrush(PlayingLineColor);
     }
 
     public override void Render(DrawingContext drawingContext) {
         int currentLine = (Application.Current as App).CelesteService.CurrentLine;
         if (currentLine >= 0) {
+            var brush = new ImmutableSolidColorBrush(Themeing.PlayingLineBgColor);
             var pixelSize = PixelSnapHelpers.GetPixelSize(TextView);
             var rect = new Rect(0, TextView.DefaultLineHeight * currentLine - TextView.ScrollOffset.Y, Bounds.Width, TextView.DefaultLineHeight);
 
-            drawingContext.FillRectangle(_playingLineBrush, PixelSnapHelpers.PixelAlign(rect, pixelSize));
+            drawingContext.FillRectangle(brush, PixelSnapHelpers.PixelAlign(rect, pixelSize));
         }
 
         base.Render(drawingContext);
--- TAS.Avalonia/Rendering/TASLineRenderer.cs
+++ TAS.Avalonia/Rendering/TASLineRenderer.cs
@@ -9,23 +9,21 @@ namespace TAS.Avalonia.Rendering;
 
 internal class TASLineRenderer : IBackgroundRenderer {
     private readonly TextArea _textArea;
-    private readonly IBrush _brush;
 
     public KnownLayer Layer => KnownLayer.Background;
 
-    public static readonly Color CurrentLineColor = Color.FromArgb(32, 180, 182, 199);
-
     public TASLineRenderer(TextArea textArea) {
         _textArea = textArea;
-        _brush = new ImmutableSolidColorBrush(CurrentLineColor);
     }
 
     public void Draw(TextView textView, DrawingContext drawingContext) {
+        var brush = new ImmutableSolidColorBrush(Themeing.CurrentLineColor);
+
         int line = _textArea.Caret.Line - 1;
 
         var pixelSize = PixelSnapHelpers.GetPixelSize(textView);
         var rect = new Rect(0, textView.DefaultLineHeight * line - textView.ScrollOffset.Y, textView.Bounds.Width, textView.DefaultLineHeight);
 
-        drawingContext.FillRectangle(_brush, PixelSnapHelpers.PixelAlign(rect, pixelSize));
+        drawingContext.FillRectangle(brush, PixelSnapHelpers.PixelAlign(rect, pixelSize));
     }
 }
